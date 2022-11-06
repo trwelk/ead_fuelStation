4a82b179a9dab045b0038227ceddf5dffab735f7
IT19163864 <64912486+ryanmlk@users.noreply.github.com>
2022-11-06 10:58:32 +0530

    test3

--- fuel-station-client/app/src/main/java/com/example/fuelstationclient/FuelStationList.java
+++ fuel-station-client/app/src/main/java/com/example/fuelstationclient/FuelStationList.java
@@ -10,11 +10,8 @@ import android.widget.SearchView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.fuelstationclient.model.FuelStation;
 import com.example.fuelstationclient.util.FuelStationListAdapter;
-import com.example.fuelstationclient.adapter.FuelStationListAdapter;
-import com.example.fuelstationclient.model.FuelStation;
 import com.example.fuelstationclient.util.WebService;
 import com.example.fuelstationclient.util.WebServiceClient;
 
@@ -35,27 +32,6 @@ public class FuelStationList extends AppCompatActivity {
         setContentView(R.layout.activity_fuel_station_list);
         init();
         getFuelStation();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.example_menu, menu);
-        MenuItem searchItem = menu.findItem(R.id.actionSearch);
-        SearchView searchView = (SearchView) searchItem.getActionView();
-        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-        return true;
     }
 
     private void init() {
